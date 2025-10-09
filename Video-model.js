da0709b6574af4fcd38900aba893deb38ce93399
Rajender Soni <sonirajender507@gmail.com>
2025-10-09 08:31:22 +0530

    Create Video-model.js

---
+++ Video-model.js
@@ -0,0 +1,38 @@
+import mongoose , {Schema} from mongoose;
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile : {
+        type : String,  //cloudinary url
+        required : true
+    },
+    thumbnail : {
+        type : String,  //cloudinary url
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : Number,  //cloudinary url
+        required : true
+    },
+    views : {
+        type : Number,
+        default : 0
+    },
+    isPublished : {
+        type : Boolean,
+        default : true,
+    },
+    owner : {
+        type : Schema.Types.ObjectId,
+        ref : "User"
+    }
+    
+},{Timestamp : true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Vedio = mongoose.model("Video", videoSchema)
